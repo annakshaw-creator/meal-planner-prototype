48fdb0d9ff0fd12c7548a770bf63f2c97490188c
annakshaw-creator <annakshaw@gmail.com>
2026-05-02 12:48:32 -0700

    Add service worker for network-first caching

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'meal-planner-v1';
+
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
