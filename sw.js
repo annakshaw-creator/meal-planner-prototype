028e1c8780741907fbf98178d47da651c4abde03
annakshaw-creator <annakshaw@gmail.com>
2026-05-10 12:36:50 -0700

    Bump SW cache version to force refresh on phone

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'meal-planner-v1';
+const CACHE = 'meal-planner-v2';
 
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
